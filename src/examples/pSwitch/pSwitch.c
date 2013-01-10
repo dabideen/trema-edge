ff9414809f10704d5030089536c10814b3212c2f
dabideen <dabideen@pc-0.d3.pgeni-gpolab-bbn-com.emulab.net>
2013-01-10 13:13:42 -0700

    clean up

--- src/examples/pSwitch/pSwitch.c
+++ src/examples/pSwitch/pSwitch.c
@@ -24,14 +24,6 @@
 #include <assert.h>
 #include "trema.h"
 
-typedef struct {
-  struct key {
-    uint8_t mac[ OFP_ETH_ALEN ];
-    uint64_t datapath_id;
-  } key;
-  uint32_t port_no;
-  time_t last_update;
-} forwarding_entry;
 
 
 time_t
@@ -44,146 +36,6 @@ now() {
  * packet_in event handler
  ********************************************************************************/
 
-static const int MAX_AGE = 300;
-
-
-static bool
-aged_out( forwarding_entry *entry ) {
-  if ( entry->last_update + MAX_AGE < now() ) {
-    return true;
-  }
-  else {
-    return false;
-  };
-}
-
-
-static void
-age_forwarding_db( void *key, void *forwarding_entry, void *forwarding_db ) {
-  if ( aged_out( forwarding_entry ) ) {
-    delete_hash_entry( forwarding_db, key );
-    xfree( forwarding_entry );
-  }
-}
-
-
-static void
-update_forwarding_db( void *forwarding_db ) {
-  foreach_hash( forwarding_db, age_forwarding_db, forwarding_db );
-}
-
-/*
-static void
-learn( hash_table *forwarding_db, struct key new_key, uint32_t port_no ) {
-  forwarding_entry *entry = lookup_hash_entry( forwarding_db, &new_key );
-
-  if ( entry == NULL ) {
-    entry = xmalloc( sizeof( forwarding_entry ) );
-    memcpy( entry->key.mac, new_key.mac, OFP_ETH_ALEN );
-    entry->key.datapath_id = new_key.datapath_id;
-    insert_hash_entry( forwarding_db, &entry->key, entry );
-  }
-  entry->port_no = port_no;
-  entry->last_update = now();
-}
-
-
-static void
-do_flooding( packet_in packet_in, uint32_t in_port ) {
-  openflow_actions *actions = create_actions();
-  append_action_output( actions, OFPP_ALL, OFPCML_NO_BUFFER );
-
-  buffer *packet_out;
-  if ( packet_in.buffer_id == OFP_NO_BUFFER ) {
-    buffer *frame = duplicate_buffer( packet_in.data );
-    fill_ether_padding( frame );
-    packet_out = create_packet_out(
-      get_transaction_id(),
-      packet_in.buffer_id,
-      in_port,
-      actions,
-      frame
-    );
-    free_buffer( frame );
-  }
-  else {
-    packet_out = create_packet_out(
-      get_transaction_id(),
-      packet_in.buffer_id,
-      in_port,
-      actions,
-      NULL
-    );
-  }
-  send_openflow_message( packet_in.datapath_id, packet_out );
-  free_buffer( packet_out );
-  delete_actions( actions );
-}
-
-static void
-send_packet( uint32_t destination_port, packet_in packet_in, uint32_t in_port ) {
-  info("got here");
-  uint32_t dest = destination_port;
-  oxm_matches *match = create_oxm_matches();
-  append_oxm_match_eth_type( match, 0x0806);
-  if(in_port == 1){
-      info( "config: ARP in 1 -> out 2");
-      append_oxm_match_in_port( match, 2);
-      dest = 1;
-  }
-  else{
-      info( "config: ARP in 2 -> out 1");
-      append_oxm_match_in_port( match, 1);
-      dest = 2;
-  }
-
-  openflow_actions *actions = create_actions();
-  append_action_output( actions, dest, OFPCML_NO_BUFFER );
-
-  openflow_instructions *insts = create_instructions();
-  append_instructions_apply_actions( insts, actions );
-
-  buffer *flow_mod = create_flow_mod(
-    get_transaction_id(),
-    get_cookie(),
-    0,
-    0,
-    OFPFC_ADD,
-    60,
-    0,
-    OFP_HIGH_PRIORITY,
-    packet_in.buffer_id,
-    0,
-    0,
-    OFPFF_SEND_FLOW_REM,
-    match,
-    insts
-  );
-  send_openflow_message( packet_in.datapath_id, flow_mod );
-  free_buffer( flow_mod );
-  delete_oxm_matches( match );
-  delete_instructions( insts );
-
-
-  if ( packet_in.buffer_id == OFP_NO_BUFFER ) {
-    buffer *frame = duplicate_buffer( packet_in.data );
-    fill_ether_padding( frame );
-    buffer *packet_out = create_packet_out(
-      get_transaction_id(),
-      packet_in.buffer_id,
-      in_port,
-      actions,
-      frame
-    );
-    send_openflow_message( packet_in.datapath_id, packet_out );
-    free_buffer( packet_out );
-    free_buffer( frame );
-  }
-
-
-  delete_actions( actions );
-}
-*/
 
 static void
 handle_packet_in( uint64_t datapath_id, packet_in message ) {
@@ -203,67 +55,13 @@ handle_packet_in( uint64_t datapath_id, packet_in message ) {
 
 
   info("got a packet to handle from, %d",datapath_id);
-/*
-  info("sending third config");
-  oxm_matches *match4 = create_oxm_matches();
-  append_oxm_match_eth_type( match4, 0x8847);
-//  append_oxm_match_mpls_label(match4, 4120955);
-
-  openflow_actions *actions4 = create_actions();
-  append_action_pop_mpls(actions4, 0x0800);
-  append_action_output( actions4, OFPP_ALL, OFPCML_NO_BUFFER );
-
-
-  openflow_instructions *insts4 = create_instructions();
-  append_instructions_apply_actions( insts4, actions4 );
-
-  buffer *flow_mod4 = create_flow_mod(
-    get_transaction_id(),
-    get_cookie(),
-    0,
-    0,
-    OFPFC_ADD,
-    0,
-    0,
-    OFP_HIGH_PRIORITY,
-    OFP_NO_BUFFER,
-    0,
-    0,
-    OFPFF_SEND_FLOW_REM,
-    match4,
-    insts4
-  );
-  send_openflow_message( datapath_id, flow_mod4);
-  free_buffer( flow_mod4 );
-  delete_oxm_matches( match4 );
-  delete_instructions( insts4 );
-  info("done sending");
-*/
 
 }
 
 
 /********************************************************************************
- * Start learning_switch controller.
+ * Start pSwitch controller.
  ********************************************************************************/
-
-static const int AGING_INTERVAL = 5;
-
-
-unsigned int
-hash_forwarding_entry( const void *key ) {
-  return hash_mac( ( ( const struct key * ) key )->mac );
-}
-
-
-bool
-compare_forwarding_entry( const void *x, const void *y ) {
-  const forwarding_entry *ex = x;
-  const forwarding_entry *ey = y;
-  return ( memcmp( ex->key.mac, ey->key.mac, OFP_ETH_ALEN ) == 0 )
-           && ( ex->key.datapath_id == ey->key.datapath_id );
-}
-
 
 static void
 handle_switch_ready( uint64_t datapath_id, void *user_data ) {
@@ -414,8 +212,6 @@ int
 main( int argc, char *argv[] ) {
   init_trema( &argc, &argv );
 
-  hash_table *forwarding_db = create_hash( compare_forwarding_entry, hash_forwarding_entry );
-  add_periodic_event_callback( AGING_INTERVAL, update_forwarding_db, forwarding_db );
   set_packet_in_handler( handle_packet_in, forwarding_db );
 
   set_switch_ready_handler( handle_switch_ready, NULL );
